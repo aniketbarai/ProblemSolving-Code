5238f965d0cc4e76be86937a65c697e5967fb714
Aniket Barai <aniketbarai677@gmail.com>
2025-07-11 11:57:37 +0530

    Add files via upload

---
+++ FIZZBUZZ2301.py
@@ -0,0 +1,7 @@
+# Here you have three captians so fans will give vote who is best captain. so here, we have to print yes if dhoni won otherwise print no..
+dhoni,rohit,kohli =map(int , input("").split())
+if(dhoni > rohit and dhoni > kohli):
+    print("Yes")
+else:
+    print("No")
+    
