8dba5cbac3fcc9f2fae572200ae4c1e2407d387e
Aniket Barai <aniketbarai677@gmail.com>
2025-08-03 21:36:12 +0530

    Update POPCORN.java

--- POPCORN.java
+++ POPCORN.java
@@ -2,30 +2,28 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-    public static void main(String[] args)
-    {
+class Codechef{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int testCase = sc.nextInt();
         while (testCase--> 0) {
             int rows = 3;
             int cols = 2;
             int arr[][] = new int[rows][cols];
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols; j++) {
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-            int max = Integer.MIN_VALUE;
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < cols - 1; j++) {
-                    if (arr[i][j] + arr[i][j + 1] > max) {
-                        max = arr[i][j] + arr[i][j + 1];
-                    }
-                }
-            }
-            System.out.println(max);
+      for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+              arr[i][j] = sc.nextInt();
+         }
+      }
+         int max = Integer.MIN_VALUE;
+  for (int i = 0; i < rows; i++) {
+   for (int j = 0; j < cols - 1; j++) {
+                if (arr[i][j] + arr[i][j + 1] > max) {
+      max = arr[i][j] + arr[i][j + 1];
+               }
+   }
+ }
+         System.out.println(max);
         }
     }
 }
